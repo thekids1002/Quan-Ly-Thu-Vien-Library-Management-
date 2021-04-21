f6bafc649c52db3d5cc5033d658df462a7eaf98d
gameooff111111 <75366637+gameooff111111@users.noreply.github.com>
2021-04-21 09:30:09 +0700

    Thay đổi DAL , Bus Phiếu Nhập

--- QuanLyThuVien_3Layer/src/BUS/PhieuNhapBUS.java
+++ QuanLyThuVien_3Layer/src/BUS/PhieuNhapBUS.java
@@ -1,5 +1,40 @@
 package BUS;
 
-public class PhieuNhapBUS {
+import java.util.ArrayList;
 
+import DAL.PhieuMuonDAL;
+import DAL.PhieuNhapDAL;
+import DTO.PhieuMuon;
+import DTO.PhieuNhap;
+
+public class PhieuNhapBUS {
+	PhieuNhapDAL pmdal = new PhieuNhapDAL(); 
+	private ArrayList<PhieuNhap> listpm = new ArrayList<PhieuNhap>();
+
+	public ArrayList<PhieuNhap> getdanhsachphieunhap() {
+
+		return listpm = pmdal.getdanhsachphieunhap();
+	}
+
+	public int thempm(PhieuNhap pm) {
+		return pmdal.ThemPN(pm) ; 
+	}
+
+	public int suapm(PhieuNhap pm) {
+		return pmdal.SuaPn(pm) ;
+	}
+
+	public int xoapm(PhieuNhap pm) {
+		return pmdal.xoanxb(pm); 
+	}
+
+	public static PhieuNhapBUS iBus = null;
+
+	public static PhieuNhapBUS gI() {
+		if (iBus == null) {
+			iBus = new PhieuNhapBUS();
+		}
+
+		return iBus;
+	}
 }
--- QuanLyThuVien_3Layer/src/DAL/PhieuNhapDAL.java
+++ QuanLyThuVien_3Layer/src/DAL/PhieuNhapDAL.java
@@ -1,5 +1,105 @@
 package DAL;
 
-public class PhieuNhapDAL {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import DTO.PhieuNhap;
+import DTO.TacGiaDTO;
+
+public class PhieuNhapDAL {
+	public static ArrayList<PhieuNhap> getdanhsachphieunhap() {
+		try {
+			String sql = "select * from phieunhap";
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			ArrayList<PhieuNhap> dsl = new ArrayList<PhieuNhap>();
+			while (rs.next()) {
+
+				PhieuNhap tg = new PhieuNhap();
+				tg.setMaPhieuNhap(rs.getInt("MaPhieuNhap"));
+				tg.setMaNhaCung(rs.getInt("MaNCC"));
+				tg.setMaNhanVien(rs.getInt("MaNV"));
+				tg.setNgayNhap(rs.getString("NgayNhap"));
+				dsl.add(tg);
+			}
+
+			return dsl;
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
+
+	}
+
+	// @SuppressWarnings("null")
+	public static int ThemPN(PhieuNhap ke) {
+		int i = -1;
+		String sql = "insert into phieunhap (MaNCC,MaNV,NgayNhap) values(?,?,?)";
+
+		try {
+
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement pstm = conn.prepareStatement(sql);
+			pstm.setInt(1, ke.getMaNhaCung());
+			pstm.setInt(2, ke.getMaNhanVien());
+			pstm.setString(3, ke.getNgayNhap());
+			i = pstm.executeUpdate();
+			conn.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			// dreturn null;
+		}
+
+		return i;
+	}
+
+	public static int SuaPn(PhieuNhap ke) {
+		int i = -1;
+		String sql = "update phieunhap set mancc = ? , manv = ?, ngaynhap = ?  where MaPhieuNhap = ?";
+
+		try {
+
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement pstm = conn.prepareStatement(sql);
+			pstm.setInt(1, ke.getMaNhaCung());
+			pstm.setInt(2, ke.getMaNhanVien());
+			pstm.setString(3, ke.getNgayNhap());
+			pstm.setInt(4, ke.getMaPhieuNhap());
+
+			// System.out.println(ke.getViTri());
+			i = pstm.executeUpdate();
+			conn.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			// dreturn null;
+		}
+
+		return i;
+	}
+
+	public static int xoanxb(PhieuNhap ke) {
+		int i = -1;
+		String sql = "delete from phieunhap where maphieunhap = ?";
+
+		try {
+
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement pstm = conn.prepareStatement(sql);
+			pstm.setInt(1, ke.getMaPhieuNhap());
+			// System.out.println(ke.getViTri());
+			i = pstm.executeUpdate();
+			conn.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			// dreturn null;
+		}
+
+		return i;
+	}
 }
